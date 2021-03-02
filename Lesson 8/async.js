5a3ba08b6e94f88a20b138d4a2e07ba1d79b9cde
Nguyễn Đoàn Duy Khanh <emonno13@gmail.com>
2021-03-03 00:12:36 +0700

    'add_homework_8'

--- Lesson 8/async.js
+++ Lesson 8/async.js
@@ -9,6 +9,12 @@
         - Trong các chương trình bất đồng bộ, X2 có thể chạy trước trong khi X1 đang thực hiện
  */
 
+// setTimeout(() => {
+//     console.log(3);
+// }, 3000); 
+// console.log(1);
+// console.log(2);
+
 /**
  * Event Loop && Call Stack && Callback Queue && Wed Apis
     ex: http://latentflip.com/loupe/
@@ -34,15 +40,29 @@
                 Cách 1: function(){}  
                 Cách 2 : () => {}
 */
-// function doHomework(subject, callback) {
+// function doHomework(subject) {
 //     alert(`Starting my ${subject} homework.`);
-//     callback();
 // }
-// doHomework('math', function() {
-//     alert('Finished my homework');
+// doHomework('math');
+
+// function doHomework(subject, callback) {
+//     console.log(`Starting my ${subject} homework.`);
+//     return callback(subject);
+// }
+// function finish(subject) {
+//     console.log(`Finished my ${subject} homework`)
+// };
+
+// doHomework('history', finish)
+// doHomework('math', function (subject) {
+//     console.log(`Finished my ${subject} homework`);
 // });
+// doHomework('chemistry', (subject) => {
+//     console.log(`Finished my ${subject} homework`);
+// })
 
 // document.getElementById('myBtn').addEventListener('click', displayDate)
+
 // function displayDate() {
 //     document.getElementById('demo').innerHTML = Date()
 // }
@@ -50,6 +70,50 @@
 // fs.readFile('demo.txt', (err, data) => {
 //   console.log(data)
 // })
+
+
+// [1,2,3].map(item => console.log(item))
+
+// setTimeout(() => {
+//     console.log('Hello')
+// }, 3000);
+// setTimeout(function () {
+//     console.log('Everyone')
+// }, 1000);
+
+// Tiến trình đồng bộ - Sync
+// console.log(1);
+// console.log(2);
+// console.log(3);
+
+// Tiến trình bất đồng bộ - Async
+// setTimeout(() => {
+//     console.log(1);
+// }, 3000);
+// setTimeout(() => {
+//     console.log(2);
+// }, 2000);
+// setTimeout(() => {
+//     console.log(3);
+// }, 1000);
+// setTimeout(() => {
+//     console.log(4);
+// }, 500);
+
+// setTimeout(() => {
+//     console.log(1);
+//     setTimeout(() => {
+//         console.log(2);
+//         setTimeout(() => {
+//             console.log(3);
+//             setTimeout(() => {
+//                 console.log(4);
+//             }, 500);
+//         }, 1000);
+//     }, 2000);
+// }, 3000);
+
+// https://freetuts.net/callback-function-la-gi-callback-trong-javascript-759.html
 
 /**
  * Promise ES6
@@ -69,7 +133,7 @@
 ///const doSomething = new Promise(excute);
 
 
-// let isKill = false;
+// let isKill = true;
 // const killMonster = new Promise(function (resolve, reject) {
 //     // Thực thi các tác vụ bất đồng bộ ở đây
 //     if (isKill) {
@@ -80,88 +144,228 @@
 // killMonster
 //     .then(data => console.log(data))
 //     .catch(err => console.log(err))
+// console.log(killMonster)
 
 // const resolveSomething = Promise.resolve(12);
-// resolveSomething.then(data => console.log(data))
+// resolveSomething
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
 
 // const rejectSomething = Promise.reject(false);
-// rejectSomething.then(data => console.log(data)).catch(err => console.log(err))
+// rejectSomething
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
 
-let money = 6000;
-const buyBag = new Promise(function (resolve, reject) {
-    setTimeout(()=> {
-        money > 5000 ? resolve('Buy Gucci bag') : reject('Buy Channel bag');
-    }, 1000)
-})
+let money = 500;
 
-const goShopping = new Promise(function (resolve, reject) {
-    setTimeout(()=> {
-        money > 1000 ? resolve(true) : reject(false);
-    }, 2000)
-})
+// const goShopping = new Promise(function (resolve, reject) {
+//     setTimeout(()=> {
+//         money > 1000 ? resolve(true) : reject(false);
+//     }, 2000)
+// })
+
+// const goSwimming = function () {
+//     return new Promise(function (resolve, reject) {
+//         setTimeout(() => {
+//             money > 1000 ? resolve(true) : reject(false);
+//         }, 2000)
+//     })
+// };
+
 // goShopping
 //     .then(data => console.log(data))
 //     .catch(err => console.log(err))
+
+// goSwimming()
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
+
 // console.log('Watch goShopping: ', goShopping);
-
-
 
 /** Promise vs Callback */
 
 // Callback Hell
-function sum(a, b, callback) {
-    let result = a + b;
-    if (!Number(result)) return callback(new Error('Can not cal SUM'))
-    return callback(result);
-};
+// function sum(a, b, callback) {
+//     let result = a + b;
+//     if (!Number(result)) return callback(new Error('Can not cal SUM'))
+//     return callback(result);
+// };
 
-sum(1, 2, function(data) {
-    const result = data;
-    console.log(result);
-});
+// sum(1, 2, function(data) {
+//     console.log(data);
+// });
 
-sum(1, 2, data => {
-    const result = data;
-    console.log(result);
-});
+// sum(1, 2, data => {
+//     console.log(data);
+// });
 
-sum(1, 2, data1 => {
-    const result1 = data1;
-    sum(result1, result1, data2 => {
-        const result2 = data2;
-        sum(result2, result2, data3 => {
-            const result3 = data3
-            sum(result3, result3, data3 => {
-                console.log(data3)
-            })
-        })
-    })
-});
+// sum(1, 2, data1 => {
+//     const result1 = data1;
+//     sum(result1, result1, data2 => {
+//         const result2 = data2;
+//         sum(result2, result2, data3 => {
+//             const result3 = data3
+//             sum(result3, result3, data3 => {
+//                 console.log(data3)
+//             })
+//         })
+//     })
+// });
 
 // Promise
-function sum02(a, b) {
-    let result = a + b;
-    if (!Number(result)) return Promise.reject('Can not cal SUM')
-    return Promise.resolve(result);
-}
-sum02(1, 2)
-    .then((result) => {
-        return result + result;
-    })
-    .then(result1 => {
-        return result1 + result1;
-    })
-    .then(result2 => {
-        return result2 + result2;
-    })
-    .then(result3 => {
-        console.log(result3)
-    })
-    .catch((err) => {
-        console.log(err)
-    });
+// function sum02(a, b) {
+//     let result = a + b;
+//     // if (!Number(result)) return Promise.reject('Can not cal SUM')
+//     // return Promise.resolve(result);
+//     return new Promise(function (resolve, reject) {
+//         !Number(result) ? reject('Can not calculate sum') : resolve(result)
+//     })
+// };
 
+// sum02(1, 2)
+//     .then((result) => {
+//         return result + result;// Promise.resolve(result)
+//     })
+//     .then(result1 => {
+//         return result1 + result1; // Promise.resolve(result)
+//     })
+//     .then(result2 => {
+//         return result2 + result2;
+//     })
+//     .then(result3 => {
+//         console.log(result3)
+//     })
+//     .catch((err) => {
+//         console.log(err) // Promise.rejected('Can not cal SUM')
+//     });
+
+
+
+/** Callback && Promise in Call API */
+
+// Problem
+// function getUserByUsername(username) {
+// 	setTimeout(function() {
+// 		////query database ...
+// 		let userId = 1;
+// 		console.log('find userId search is  :'+userId);
+// 		return userId;
+// 	}, 3000);
+// }
+
+// function getPostByUserId(userId) {
+	
+// 	setTimeout(function() {
+// 		//query database ...
+// 		let postId = 2;
+// 		console.log('find postId search is  :'+postId);
+// 		return postId;
+// 	}, 2000);
+// }
+
+// function getPostCommentByPostId(postId) {
+// 	setTimeout(function() {
+// 		//query database ...
+// 		let commentId = 3;
+// 		console.log('find commentId search is  :'+commentId);
+// 		return commentId;
+// 	}, 1000);
+// }
+
+let userId = getUserByUsername('Khanh');
+let postId = getPostByUserId(userId);
+let commentId = getPostCommentByPostId(postId);
+
+// Using Callback
+// function getUserByUsername(username, callback) {
+// 	setTimeout(function() {
+// 		userId = 1;
+// 		console.log('find userId search is  :'+userId);
+// 		return callback(null, userId);
+// 	}, 3000);
+// }
+
+// function getPostByUserId(userId, callback) {
+	
+// 	setTimeout(function() {
+// 		var postId = 2;
+// 		console.log('find postId search is  :'+postId);
+// 		return callback(null, postId);
+// 	}, 2000);
+// }
+
+// function getCommentByPostId(postId) {
+	
+// 	setTimeout(function() {
+// 		var commentId = 3;
+// 		console.log('find commentId search is  :'+commentId);
+// 		return commentId;
+// 	}, 2000);
+// }
+
+// getUserByUsername('Khanh', function(err, userId) {
+// 	 getPostByUserId(userId, function(err, postId){
+// 		 getCommentByPostId(postId);
+// 	 });
+// });
+ 
+// Using Promise
+
+var getUserIdByUsername = (username) => {
+    return new Promise(function (resolve, reject) {
+		setTimeout(function() {
+			if(typeof username == 'number') { 
+				// .... do something with username
+				return reject('error occurred with function getUserIdByUsername!');
+			}
+			var userId = 1;
+			console.log('find user id search is  :'+userId);
+			return resolve(userId);
+		}, 1000);
+	})
+}; 
+
+var getPostIdByUserId = (userId) => {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            // .... do something with userId
+			if(typeof userId != 'number') { 
+				return reject('error occurred with function getPostIdByUserId!');
+			}
+			var postId = 2;
+			console.log('find pos id search is  :'+postId);
+			return resolve(postId);
+		}, 2000);
+	})
+};
+
+var getCommentIdByPostId = (postId) => {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            // .... do something with postID
+			if(typeof postId != 'number') { 
+				return reject('error occurred with function getCommentIdByPostId!');
+			}
+			var commentId = 3;
+			console.log('find comnent id search is  :'+commentId);
+			resolve(commentId);
+		}, 3000);
+	})
+};
+
+getUserIdByUsername('Khanh')
+    .then((userId) => getPostIdByUserId(userId))
+    .then((postId) => getCommentIdByPostId(postId))
+    .then((commentId) => {
+        // continue promise
+        console.log(commentId)
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 
 /**
  * Async/Await ES7
  */
+
+//  https://itphutran.com/tu-callback-den-promise-async-await-trong-javascript/
